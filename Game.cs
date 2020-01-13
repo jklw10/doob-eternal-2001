0423f5fd92a7e6eccf0646f83d4de803e36b0f55
jklw10 <112possu@gmail.com>
2020-01-14 01:01:43 +0200

    Add files via upload
    changed shoot towards to a more modular system

--- Game.cs
+++ Game.cs
@@ -94,12 +94,13 @@ namespace Doob_eternal_2001
             List<RayCast> casts = new List<RayCast>();
             for (int x = 0; x < screenWidth; x++)
             {
-                foreach (RayCast cast in ShootTowards(x))
+                Vector CameraRay = Vector.RadiansToVector((player.Rotation - fov * deg2rad) + (x / (screenWidth * 1f)) * fov * deg2rad, 1);
+                foreach (RayCast cast in ShootTowards(player.Position,CameraRay))
                 {
-
+                    
                     double wallDistance = Math.Sqrt(cast.Length * cast.Length + cast.Z * cast.Z);
                     double depthMulti = screenWidth / wallDistance;
-                    lineHeight = (int)(depthMulti * cast.WallHeight);
+                    lineHeight = (int)(depthMulti * cast.WallHeight*(1f+Math.Cos(CameraRay.Angle-player.Rotation+((fov/2f)*deg2rad))));
 
                     line.Corners[0] = (x, depthMulti * cast.Z + (screenHeight - lineHeight) / 2);
                     line.Corners[1] = (x, depthMulti * cast.Z + (screenHeight + lineHeight) / 2);
@@ -139,27 +140,9 @@ namespace Doob_eternal_2001
         private void Game_Load(object sender, EventArgs e)
         {
             Shape tri   = new Shape(1, 1,Color.Blue, (1, 0), (1, 1), (0, 1));
-            Shape rect  = new Shape(1, 1,Color.Blue, (1, 0), (0, 0), (0, 1), (1, 1));
-            int cornerc = 8;
-            Vector[] starCorners = new Vector[cornerc];
-
-            double starRots = (360 / cornerc) * deg2rad;
-            for (int i = 0; i < starCorners.Length; i++)
-            {
-                //if (i % 2 == 0)
-                //{ 
-                //    starCorners[i] = Vector.RotateVector((1 , 0),starRots*i);
-                //}
-                //else
-                {
-                    starCorners[i] = Vector.RotateVector((0.5 + (i%2), 0), starRots * i);
-                }
-            }
-            Shape treePart = new Shape(0, 1, new Shader(125, 0, 0, 125), starCorners);
             tri.Scale((1,1),0.2);
-            
             Shape rect2 = new Shape(-1, 1, new Shader(255,0,125,0),(1, 0), (0, 0), (0, 1), (1, 1));
-            tri.Move((2, 2), 0);
+            
             rect2.Scale((20, 20), 5);
             ToDraw.Add(rect2);
             ToDraw.Add(tri);
@@ -170,7 +153,7 @@ namespace Doob_eternal_2001
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        Queue<RayCast> ShootTowards(double x)
+        Queue<RayCast> ShootTowards(Vector Position, Vector RayDirection)
         {
             double RayHitDistance = 0;
             Queue<RayCast> casts = new Queue<RayCast>();
@@ -191,11 +174,10 @@ namespace Doob_eternal_2001
                             b = 0;
                         }
                         
-                        Vector CameraRay = Vector.RadiansToVector((player.Rotation - fov * deg2rad) + (x / (screenWidth *1)) * fov * deg2rad, 1);
 
                         double CurrentRayHitDistance = 0;
 
-                        if (Vector.RayCast(player.Position, CameraRay, shape.Corners[a], shape.Corners[b], out Vector intersect))
+                        if (Vector.RayCast(player.Position, RayDirection, shape.Corners[a], shape.Corners[b], out Vector intersect))
                         {
                             CurrentRayHitDistance = (intersect - player.Position).Magnitude;
                             castsUnsorted.Add(new RayCast(CurrentRayHitDistance, ToDraw[i].WallHeight, ToDraw[i].Z-player.Height, ToDraw[i].Color));
@@ -223,41 +205,11 @@ namespace Doob_eternal_2001
         {
             
             bool blocked = false;
-            double shortestDist = double.MaxValue;
-            for (int i = 0; i < ToDraw.Count; i++)
+            Queue<RayCast> casts = ShootTowards(thing.Position, direction);
+            double l = casts.Last().Length;
+            if (l >= direction.Magnitude)
             {
-                Shape shape = ToDraw[i];
-                //go through all corners in a shape
-                foreach (Vector corner in shape.Corners)
-                {
-                    //connect this corner to the next corner for a line to intersect check with.
-                    for (int a = 0; a < shape.Corners.Length; a++)
-                    {
-                        int b = a + 1;
-                        if (a == shape.Corners.Length - 1)
-                        {
-                            b = 0;
-                        }
-
-
-                        double CurrentRayHitDistance;
-
-                        if (Vector.SegmentIntersect(thing.Position, thing.Position+ direction, shape.Corners[a], shape.Corners[b], out Vector intersect))
-                        {
-                            blocked = true;
-                            CurrentRayHitDistance = (intersect - thing.Position).Magnitude;
-                            if(CurrentRayHitDistance < shortestDist)
-                            {
-                                shortestDist = CurrentRayHitDistance;
-                            }
-                        }
-                    }
-                }
-            }
-            if (blocked)
-            {
-                //thing.Position += direction.Normalize()*(shortestDist-0.0001);
-                thing.Position += direction;
+                thing.Position += Vector.RadiansToVector(direction.Angle, l - 0.01);
             }
             else
             {
---
+++ Vector3D.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Doob_eternal_2001
+{
+
+    struct Vector3D : IComparable<Vector3D>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector XY
+        {
+            get { return (X, Y); }
+            set { X = value.X; Y = value.Y; }
+        }
+        public Vector XZ
+        {
+            get { return (X, Z); }
+            set { X = value.X; Z = value.Y; }
+        }
+        public Vector YZ
+        {
+            get { return (Y, Z); }
+            set { Y = value.X; Z = value.Y; }
+        }
+
+
+        public double Magnitude
+        {
+            get { return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2)); }
+            private set { }
+        }
+
+        public Vector Angle
+        {
+            get { return VectorToRadians(this); }
+            set { }
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + "," + Z + ")";
+        }
+
+        /// <summary>
+        /// (X, Y)
+        /// </summary>
+        /// <param name="coordinate"></param>
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3D(Vector xy, double z)
+        {
+            X = xy.X;
+            Y = xy.Y;
+            Z = z;
+        }
+        /// <summary>
+        /// creates a vector of (size, size, size)
+        /// </summary>
+        /// <param name="size"></param>
+        public Vector3D(double size)
+        {
+            X = size;
+            Y = size;
+            Z = size;
+        }
+        public int CompareTo(Vector3D other)
+        {
+            // The magnitude comparison depends on the comparison of 
+            // the underlying Double values. 
+            return Magnitude.CompareTo(other.Magnitude);
+        }
+
+        public static bool operator >(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) == 1;
+        }
+
+        // Define the is less than operator.
+        public static bool operator <(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) == -1;
+        }
+
+        // Define the is greater than or equal to operator.
+        public static bool operator >=(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) >= 0;
+        }
+
+        // Define the is less than or equal to operator.
+        public static bool operator <=(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) <= 0;
+        }
+        public static bool operator ==(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) == 0;
+        }
+        public static bool operator !=(Vector3D operand1, Vector3D operand2)
+        {
+            return operand1.CompareTo(operand2) != 0;
+        }
+        public static Vector3D operator +(Vector3D a, Vector b)
+        {
+            return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z);
+        }
+        public static Vector3D operator -(Vector3D a, Vector b)
+        {
+            return new Vector3D(a.X - b.X, a.Y - b.Y, a.Z);
+        }
+        public static Vector3D operator +(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector3D operator -(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        public static Vector3D operator *(Vector3D a, double b)
+        {
+            return new Vector3D((int)(a.X * b), (int)(a.Y * b), (int)(a.Z * b));
+        }
+
+        public static Vector3D operator *(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector3D operator /(Vector3D a, double b)
+        {
+            return new Vector3D((int)Math.Round(a.X / b), (int)Math.Round(a.Y / b), (int)Math.Round(a.Z / b));
+        }
+
+        public static Vector3D operator /(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        }
+
+        public static Vector3D operator %(Vector3D a, Vector3D b)
+        {
+            return a - Floor(a / b) * b;
+        }
+
+        public static Vector3D Floor(Vector3D a)
+        {
+            return new Vector3D((int)Math.Floor(a.X), (int)Math.Floor(a.Y), (int)Math.Floor(a.Z));
+        }
+        /// <summary>
+        /// converts from a Vector to radians;
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector VectorToRadians(Vector3D v)
+        {
+            return new Vector(Math.Atan2(v.Y, v.X), Math.Atan2(v.Z, v.XY.Magnitude));
+        }
+
+        public static explicit operator string(Vector3D v)
+        {
+            return "(" + v.X + ", " + v.Y + ", " + v.Z + ")";
+        }
+
+        public static implicit operator (double,double,double)(Vector3D v)
+        {
+            return (v.X, v.Y, v.Z);
+        }
+
+        public static implicit operator Vector3D((int x, int y, int z) position)
+        {
+            return new Vector3D(position.x, position.y, position.z);
+        }//*/
+        public static implicit operator Vector3D((IntVector v, int z) position)
+        {
+            return new Vector3D(position.v.X, position.v.Y, position.z);
+        }
+    }
+}
